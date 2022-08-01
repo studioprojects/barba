1a64a8397f07ce0667c6020d1bc4206f202f3f88
StudioProjects <andy@studioprojects.com.au>
2022-08-01 22:26:10 +1000

    4 files modified

--- js/transitions.js
+++ js/transitions.js
@@ -1,65 +1,57 @@
-
 function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
 }
 
 function pageTransition() {
-    var tl = gsap.timeline();
-    tl.to(".loading-screen", {
-        duration: 0.8,
-        width: "100%",
-        left: "0%",
-        ease: "Expo.easeInOut",
-    });
+  var tl = gsap.timeline();
+  tl.to('.loading-screen', {
+    duration: 0.8,
+    width: '100%',
+    left: '0%',
+    ease: 'Expo.easeInOut',
+  });
 
-    tl.to(".loading-screen", {
-        duration: 0.6,
-        width: "100%",
-        left: "100%",
-        ease: "Expo.easeInOut",
-        delay: 0.3,
-    });
-    tl.set(".loading-screen", { left: "-100%" });
+  tl.to('.loading-screen', {
+    duration: 0.6,
+    width: '100%',
+    left: '100%',
+    ease: 'Expo.easeInOut',
+    delay: 0.3,
+  });
+  tl.set('.loading-screen', { left: '-100%' });
 }
 
 function menuAnimateOut() {
-    var tl = gsap.timeline
-        .to('.slider-top', {right: '-100%', duration: 0.5, ease: 'inOut'})
-        .to('.slider-bottom', {right: '-100%', duration: 0.3, ease: 'inOut'})
+  var tl = gsap.timeline
+    .to('.slider-top', { right: '-100%', duration: 0.5, ease: 'inOut' })
+    .to('.slider-bottom', { right: '-100%', duration: 0.3, ease: 'inOut' });
 }
 
 barba.init({
-    
+  sync: true,
 
-    transitions: [
-        {
-            sync: true,
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
 
-            async leave(data) {
-                const done = this.async();
+        pageTransition();
+        await delay(1000);
+        done();
+      },
 
-                $(data.next.container).addClass('fixed');
+      async enter(data) {
+        menuAnimateOut();
+      },
 
-                pageTransition();
-                await delay(1000);
-                done();
-            },
-
-            async enter(data) {
-
-                $(data.next.container).removeClass('fixed');
-
-                menuAnimateOut();
-            },
-
-            async once(data) {
-                menuAnimateOut();
-            },
-        },
-    ],
+      async once(data) {
+        menuAnimateOut();
+      },
+    },
+  ],
 });
